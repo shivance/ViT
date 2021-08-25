e9663cdacbefd2adb7f1acd8f28f0189dd69c268
Anshuman Mishra <mishra_941920@student.nitw.ac.in>
2021-08-25 11:46:36 +0530

    pytorch transformer

--- main_pt.py
+++ main_pt.py
@@ -2,6 +2,7 @@ import torch
 from torch import nn,einsum
 from einops import rearrange,repeat
 from einops.layers.torch import Rearrange
+from torch.nn.modules.activation import MultiheadAttention
 
 def pair(t):
     return t if isinstance(t, tuple) else (t,t)
@@ -34,6 +35,7 @@ class FFN(nn.Module):
 class Attention(nn.Module):
     def __init__(self,dim,heads=8,dim_head=64,dropout=0.):
         super().__init__()
+        inner_dim = dim_head*heads
         project_out = not(heads==1 and dim_head==dim)
         self.heads = heads
         self.scale = dim_head**-0.5
@@ -47,3 +49,97 @@ class Attention(nn.Module):
 
     def forward(self,x):
         b,n,_,h= *x.shape, self.heads
+        qkv = self.to_qkv(x).chunk(3,dim=-1)
+        q,k,v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d',h=h),qkv)
+        
+        dots = einsum('b h i d, b h j d -> b h i j',q,k)*self.scale
+        attn = self.attend(dots)
+        out = einsum('b h i j, b h j d -> b h i d',attn,v)
+        out = rearrange(out,'b h n d -> b n (h d)')
+
+        return self.to_out(out)
+
+
+class Transformer(nn.Module):
+    def __init__(self,dim,depth,heads,dim_head,mlp_dim,dropout=0.):
+        super().__init__()
+        self.layers = nn.ModuleList([])
+
+        for _ in range(depth):
+            self.layers.append(
+                nn.ModuleList([
+                    PreNorm(dim,Attention(dim,heads=heads,dim_head=dim_head,dropout=dropout)),
+                    PreNorm(dim,FFN(dim,mlp_dim,dropout=dropout))
+                ])
+            )
+
+    
+    def forward(self,x):
+        for attn,ff in self.layers:
+            x = attn(x)+x
+            x = ff(x)+x
+
+        return x
+
+
+class ViT(nn.Module):
+    def __init__(self,
+                *,image_size,
+                patch_size,
+                num_classes,
+                dim, depth,
+                heads, mlp_dim,
+                pool="cls", 
+                channels=3,
+                dim_head=64,
+                dropout=0.,
+                emb_dropout=0.):
+
+        super().__init__()
+        image_height, image_width = pair(image_size)
+        patch_height, patch_width = pair(patch_size)
+
+        assert image_height%patch_height==0 and image_width%patch_width==0,"Image Dimension must be divisible by patch size"
+        
+        num_patches = (image_height//patch_height) * (image_width//patch_width)
+        patch_dim = channels*patch_height*patch_width
+
+        assert pool in {'cls','mean'}, "pool type must be either cls (cls token) or mean (mean pooling)"
+
+        self.to_patch_embedding = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)',p1=patch_width,
+            p2 = patch_width),
+            nn.Linear(patch_dim,dim),
+        )
+
+        self.pos_embedding = nn.parameter.Parameter(torch.randn(1,num_patches+1,dim))
+        self.cls_token = nn.parameter.Parameter(torch.randn(1,1,dim))
+        self.dropout = nn.Dropout(emb_dropout)
+
+        self.transformer = Transformer(dim,depth,heads,dim_head,mlp_dim,dropout)
+        self.pool = pool
+        self.to_latent = nn.Identity()
+        self.mlp_head = nn.Sequential(
+            nn.LayerNorm(dim),
+            nn.Linear(dim,num_classes),
+        )
+
+    def forward(self,img):
+        x = self.to_patch_embedding(img)
+        b, n, _ = x.shape
+        
+        cls_tokens = repeat(self.cls_token, '() n d -> b n d',b = b)
+        x = torch.cat((cls_tokens,x),dim=1)
+        x += self.pos_embedding[:,:(n+1)]
+        x = self.transformer(x)
+        x = x.mean(dim=1) if self.pool == 'mean' else x[:,0]
+        x = self.to_latent(x)
+        return self.mlp_head(x)
+
+
+
+
+
+
+#model = ViT(image_size=256,patch_size=32,num_classes=1000,dim=1024,depth=6,heads=16,mlp_dim=2948,dropout=0.1)
+
